67b7405cc027ae3838747bf150967e981670b84d
Kevin@test.com <kevin@test.com>
2017-11-10 15:16:02 +0000

    section 1 complete

--- worksheet-3.go
+++ worksheet-3.go
@@ -15,14 +15,19 @@ import (
 /*Function ElizaResponse that takes in and returns a Random String
 *by referencing its array index.
  */
-func ElizaResponse() string {
-
+func ElizaResponse(str string) (string, string) {
+	human := str
 	rand.Seed(time.Now().UnixNano())
 	random := []string{"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?"}
-	return random[rand.Intn(3-0)]
+
+	return human, random[rand.Intn(3-0)]
 }
 func main() {
-	fmt.Println(ElizaResponse())
+	fmt.Println(ElizaResponse("People say I look like both my mother and father.\n"))
+	fmt.Println(ElizaResponse("Father was a teacher.\n"))
+	fmt.Println(ElizaResponse("I was my father’s favourite.\n"))
+	fmt.Println(ElizaResponse("I’m looking forward to the weekend.\n"))
+	fmt.Println(ElizaResponse("My grandfather was French!\n"))
 }
