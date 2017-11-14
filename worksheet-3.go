06b92f6d383835603cbab0df6130f46e142d8caa
Kevin@test.com <kevin@test.com>
2017-11-14 22:38:30 +0000

    section 3

--- worksheet-3.go
+++ worksheet-3.go
@@ -3,7 +3,15 @@
 //@Author Kevin Gleeson
 //URL: https://github.com/kevgleeson78/Worksheet-3
 //date 10/11/2017
-
+//Version:  1.0
+/*Sources:
+*https://gobyexample.com/regular-expressions
+*https://regexr.com/
+*https://astaxie.gitbooks.io/build-web-application-with-golang/en/07.3.html
+*https://regexone.com/lesson/letters_and_digits?
+*
+*
+ */
 package main
 
 import (
@@ -16,31 +24,47 @@ import (
 /*Function ElizaResponse that takes in and returns a Random String
 *by referencing its array index.
  */
-func ElizaResponse(str string) (string, string) {
-	human := str
+func ElizaResponse(str string) string {
 
+	response := "How do you know you are"
 	random := []string{"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?"}
 
-	matched, _ := regexp.MatchString(`(?i)\bfather\b`, human)
+	matched, _ := regexp.MatchString(`(?i)\bfather\b`, str)
 	if matched {
-
-		return human, "Why don’t you tell me more about your father?"
+		return "Why don’t you tell me more about your father?"
+	}
+	matchedIAM, _ := regexp.MatchString(`I am`, str)
+	if matchedIAM {
+		r1 := regexp.MustCompile(`((?i)I Am)`)
+		firstString := r1.ReplaceAllString(str, response)
+		r1 = regexp.MustCompile(`(\.)`)
+		secondString := r1.ReplaceAllString(firstString, "?")
+		return secondString
 	}
 	rand.Seed(time.Now().UnixNano())
 	randIndex := rand.Intn(len(random))
-	return human, random[randIndex]
-
+	return random[randIndex]
 }
 func main() {
-	fmt.Println(ElizaResponse("People say I look like both my mother and father.\n"))
+	fmt.Println("People say I look like both my mother and father.")
+	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	fmt.Println("Father was a teacher.")
+	fmt.Println(ElizaResponse("Father was a teacher."))
+	fmt.Println("I was my father’s favourite.")
+	fmt.Println(ElizaResponse("I was my father’s favourite."))
+	fmt.Println("I’m looking forward to the weekend.")
+	fmt.Println(ElizaResponse("I’m looking forward to the weekend."))
+	fmt.Println("My grandfather was French!")
+	fmt.Println(ElizaResponse("My grandfather was French!"))
+	fmt.Println("I am happy.")
+	fmt.Println(ElizaResponse("I am happy."))
+	fmt.Println("I am not happy with your responses.")
+	fmt.Println(ElizaResponse("I am not happy with your responses."))
+	fmt.Println("I am not sure that you understand the effect that your questions are having on me.")
+	fmt.Println(ElizaResponse("I am not sure that you understand the effect that your questions are having on me."))
+	fmt.Println("I am supposed to just take what you’re saying at face value?")
+	fmt.Println(ElizaResponse("I am supposed to just take what you’re saying at face value?"))
 	fmt.Println()
-	fmt.Println(ElizaResponse("Father was a teacher.\n"))
-	fmt.Println()
-	fmt.Println(ElizaResponse("I was my father’s favourite.\n"))
-	fmt.Println()
-	fmt.Println(ElizaResponse("I’m looking forward to the weekend.\n"))
-	fmt.Println()
-	fmt.Println(ElizaResponse("My grandfather was French!\n"))
 }
