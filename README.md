720e160ed716c32e8b91a5b95fedd48b36a33047
Kevin@test.com <kevin@test.com>
2017-11-22 10:44:48 +0000

    regexp statements changed and readme added.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# Worksheet-3
-Go worksheet part 3
+# Worksheet-3 Regular Expressions
+This is a repository with an example of Regular expressions with the [Go](https://golang.org/) programming language.
+
+Author: [Kevin Gleeson](https://github.com/kevgleeson78)
+
+Third year student at:[GMIT](http://gmit.ie) Galway
+
+## Cloning, compiling and running the application.
+
+1: Download [git](https://git-scm.com/downloads) to your machine if not already installed.
+
+1.1: Download [go](https://golang.org/dl/) if not already installed.
+
+2: Open git bash and cd to the folder you wish to hold the repository.
+Alternatively you can right click on the folder and select git bash here.
+This will open the git command prompt in the folder selected.
+ 
+ 3: To clone the repository type the following command in the terminal making sure you are in the folder needed for the repository.
+```bash
+>git clone https://github.com/kevgleeson78/Worksheet-3
+```
+4: To compile the application cd to the folder and type the following 
+```bash
+> go build 
+```
+This will compile and create an executable file from the .go file and give it the name of the folder.
+
+5: To run the application ensure you cd to folder the application is held.
+Type the following command
+```bash
+>./worksheet-3
+```
+6: This will run the application to the terminal window.
+
--- worksheet-3.go
+++ worksheet-3.go
@@ -11,10 +11,10 @@
 *https://regexone.com/lesson/letters_and_digits?
 *https://github.com/StefanSchroeder/Golang-Regex-Tutorial/blob/master/01-chapter1.markdown
 *https://regex101.com/r/vH0iN5/2
-*https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
 *https://stackoverflow.com/questions/10196462/regex-word-boundary-excluding-the-hyphen
 *https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
 *https://stackoverflow.com/questions/47356475/apostrophe-in-word-not-being-recognized-for-string-replace/47357958#47357958
+*https://stackoverflow.com/questions/42018775/pattern-matching-and-regular-expression-in-perl
  */
 package main
 
@@ -30,19 +30,28 @@ func Reflections(capturedString string) string {
 	//adapted from https://stackoverflow.com/questions/10196462/regex-word-boundary-excluding-the-hyphen
 	//To prevent "you're"  or any word with a "'" from getting split into three tokens
 	boundaries := regexp.MustCompile(`(\b[^\w']|$)`)
+	//tokens splits up the string into single words for the reflections array to cahge to nouns.
 	tokens := boundaries.Split(capturedString, -1)
-
-	// List the reflections.
+	// List ofreflections.
+	//It depends on the order of the words in the array on how they will be changed from the reflectString function.
 	reflections := [][]string{
-
-		{`your`, ` my`},
-		{`you're`, `I'm`},
-		{`you`, `I`},
-		{`I`, `you`},
-		{`me`, `you`},
-		{`my`, `your`},
+		{`\byou're\b`, `I'm`},
+		{`\b(?i)I'm\b`, `you're`},
+		{`\bare\b`, `am`},
+		{`\bam\b`, `are`},
+		{`\bwere\b`, `was`},
+		{`\bwas\b`, `were`},
+		{`\byou\b`, `I`},
+		{`\bi\b`, `you`},
+		{`\bme\b`, `you`},
+		{`\byour\b`, `my`},
+		{`\bmy\b`, `your`},
+		{`\byou've\b`, `I've`},
+		{`\bI've\b`, `you've`},
+		{`\bme\b`, `you`},
+		{`\byou\b`, `me`},
 	}
-
+	//Adapted from https://gist.github.com/ianmcloughlin/c4c2b8dc586d06943f54b75d9e2250fe
 	// Loop through each token, reflecting it if there's a match.
 	for i, token := range tokens {
 		for _, reflection := range reflections {
@@ -73,45 +82,80 @@ func ElizaResponse(str string) string {
 	matched {
 		return "Why don’t you tell me more about your father?"
 	}
-	r1 := regexp.MustCompile(`(?i)I'?\s*a?m`)
+	r1 := regexp.MustCompile(`(?i)I'?\s*a?m(.*)`)
 
-	//Match the words "I am" and capture for replacement
+	//Match the words "I am, Im, I'm" and capture everything else after for replacement
 	matched := r1.MatchString(str)
 
-	//condition if "I am" is matched
+	//condition if "I am, Im, I'm" is matched
 	if matched {
-		replacementString := "How do you know that you are"
+
 		//Only keep the captured part of the string
-		//Pass in everything after the captured part of the statement to the function Reflections
-		response := Reflections(r1.ReplaceAllString(str, "$1"))
-		//Concat the new opening line at the end of the function
-		return replacementString + response
+		//Pass in everything after the captured part of the statement $1 to the function Reflections
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+		//Concat the reflected sring to the opening response
+		response := "How do you know you are " + reflectString + "?"
+		//return the new string response
+		return response
 	}
-	//Capture and replace How are you
-	r1 = regexp.MustCompile(`\bHow are you\b`)
+	//Capture "why"
+	r1 = regexp.MustCompile(`(?im)^\s*Why ([^\?]*)\?*\s*$`)
+	//match "why" in the string
+	matched = r1.MatchString(str)
+	//condition for matching the string "why"
+	if matched {
+		//Only keep the captured part of the string
+		//Pass in everything after the captured part of the statement $1 to the function Reflections
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+		//Concat the reflected sring to the opening response
+		response := "Why do you ask " + reflectString + "?"
+
+		//return the new string response
+		return response
+	}
+	//Capture and replace "my name is"
+	r1 = regexp.MustCompile(`(?im)^\s*My name is ([^\?]*)\?*\s*$`)
+	//match the phrase from the regular expression
 	matched = r1.MatchString(str)
 	if matched {
-		replacementString := "Why am I"
 		//Only keep the captured part of the string
-		//Pass in everything after the captured part of the statement to the function Reflections
-		response := Reflections(r1.ReplaceAllString(str, "$1"))
-		//Concat the new opening line at the end of the function
-		return replacementString + response
+		//Pass in everything after the captured part of the statement $1 to the function Reflections
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+		//Concat the reflected sring to the opening response
+		response := "Hello " + reflectString + " How are you today?"
+		//return the new string response
+		return response
 	}
-	//Capture and replace How are you
-	r1 = regexp.MustCompile(`\bMy name is\b`)
+	//Capture and replace "How are you"
+	r1 = regexp.MustCompile(`(?im)^\s*how are you ([^\?]*)\?*\s*$`)
+	//match the phrase from the regular expression
 	matched = r1.MatchString(str)
 	if matched {
-		replacementString := "Hello"
 		//Only keep the captured part of the string
-		//Pass in everything after the captured part of the statement to the function Reflections
-		response := Reflections(r1.ReplaceAllString(str, "$1"))
-		//Concat the new opening line at the end of the function
-		return replacementString + response
+		//Pass in everything after the captured part of the statement $1 to the function Reflections
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+		//Concat the reflected sring to the opening response
+		response := "How does " + reflectString + "effect you?"
+		//return the new string response
+		return response
+	}
+	//Capture and replace  "are you"
+	r1 = regexp.MustCompile(`(?im)^\s*are you ([^\?]*)\?*\s*$`)
+	//match the phrase from the regular expression
+	matched = r1.MatchString(str)
+	if matched {
+		//Only keep the captured part of the string
+		//Pass in everything after the captured part of the statement $1 to the function Reflections
+		reflectString := Reflections(r1.ReplaceAllString(str, "$1"))
+		//Concat the reflected sring to the opening response
+		response := "I have'nt decided if I am " + reflectString
+		//return the new string response
+		return response
 	}
 	//Get random number from the length of the array of random struct
 	//an array of strings for the random response
-	randomResponse := []string{"I’m not sure what you’re trying to say. Could you explain it to me?",
+	randomResponse := []string{
+		"I’m not sure what you’re trying to say. Could you explain it to me?",
 		"How does that make you feel?",
 		"Why do you say that?"}
 	//Return a random index of the array
@@ -120,9 +164,13 @@ func ElizaResponse(str string) string {
 }
 
 func main() {
+	//To get a random value for the array index of randomResponse
 	rand.Seed(time.Now().UTC().UnixNano())
+	//Print out the test inputs
 	fmt.Println("People say I look like both my mother and father.")
+	//Input to elizaResponse function
 	fmt.Println(ElizaResponse("People say I look like both my mother and father."))
+	//Empty space for next output
 	fmt.Println()
 	fmt.Println("Father was a teacher.")
 	fmt.Println(ElizaResponse("Father was a teacher."))
@@ -154,12 +202,16 @@ func main() {
 	fmt.Println(ElizaResponse("How are you feeling."))
 	fmt.Println()
 	//Test Input Two
-	fmt.Println("I am your friend.")
-	fmt.Println(ElizaResponse("I am your friend."))
+	fmt.Println("Are you my friend?")
+	fmt.Println(ElizaResponse("Are you my friend?"))
 	fmt.Println()
 	//Test input Three
 	fmt.Println("My name is Kevin")
 	fmt.Println(ElizaResponse("My name is Kevin"))
+	fmt.Println()
+	//Test input Four
+	fmt.Println("Why are you not answering my questions properly?")
+	fmt.Println(ElizaResponse("Why are you not answering my questions properly?"))
 	fmt.Println()
 
 }
